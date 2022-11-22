d7cb8c877777dce65e41e1a9a2083e39ba765ef4
Carlos Henrique <carlosjesus@discente.ufg.br>
2022-11-22 00:58:54 -0300

    feature-4-detalhe-dos-herois

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,18 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id:1, name: 'Iron Man'},
+  {id:2, name: 'Captain America'},
+  {id:3, name: 'Hulk'},
+  {id:4, name: 'Black Widow'},
+  {id:5, name: 'Hawkeye'},
+  {id:6, name: 'Thor'},
+  {id:7, name: 'Scarlet Witch'},
+  {id:8, name: 'Quicksilver'},
+  {id:9, name: 'The Vision'},
+  {id:10, name: 'Spider-Man'},
+  {id:11, name: 'Black Panther'},
+  {id:12, name: 'War Machine'},
+];
+
+//não esquecer do export para usar essa constante
