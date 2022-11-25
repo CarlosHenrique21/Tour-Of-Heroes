6fdc90a12efc13aeb74a884cadeed4ed73a23ff5
Carlos Henrique <carlosjesus@discente.ufg.br>
2022-11-25 09:53:35 -0300

    Adicionando o core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,14 @@ import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MaterialModule } from './material/material.module';
-
+import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
     DashboardComponent,
   ],
   imports: [
@@ -24,8 +20,7 @@ import { MaterialModule } from './material/material.module';
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule,
-    FlexLayoutModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
